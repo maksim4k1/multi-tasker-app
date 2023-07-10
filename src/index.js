9598a72177dd8bd4dfbd54db728ab9712d0ecd2f
maksim4k1 <maksim4k1@gmail.com>
2023-07-10 14:08:56 +0700

    added gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import './styles/index.css';
 import ScrollToTop from "./components/ScrollToTop";
@@ -11,10 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <ScrollToTop/>
         <App/>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
